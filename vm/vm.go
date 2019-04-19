9b4cfdd736d7ec852250cf7c2555445e1aad4f10
mattpollack <matt.pck@gmail.com>
2019-04-19 15:47:04 -0400

    Simplified operations

--- vm/vm.go
+++ vm/vm.go
@@ -35,8 +35,18 @@ func NewVM(prog *ByteBuffer) *VM {
 	}
 }
 
-func (v *VM) Pop() Instruction {
-	return nil
+func (v *VM) PopData() Data {
+	data := Data{}
+	data.Decode(v.stack.Slice(0, -1))
+	v.stack = v.stack.Slice(data.Size(), -1)
+
+	return data
+}
+
+func (v *VM) PushData(data Data) {
+	tempBuffer := NewByteBuffer()
+	data.Emit(tempBuffer)
+	v.stack.Push(tempBuffer.Bytes())
 }
 
 func (v *VM) NextOP() OP {
@@ -45,6 +55,14 @@ func (v *VM) NextOP() OP {
 	v.pp += op.Size()
 
 	return op
+}
+
+func (v *VM) NextData() Data {
+	data := Data{}
+	data.Decode(v.Prog.Slice(v.pp, -1))
+	v.pp += data.Size()
+
+	return data
 }
 
 func (v *VM) Error(err error) int {
@@ -61,91 +79,61 @@ func (v *VM) Step() int {
 
 	switch v.NextOP().Kind {
 	case INS_PUSH:
-		arg := &Data{}
-		arg.Decode(v.Prog.Slice(v.pp, -1))
-
-		// Use a bytebuffer to extract bytes
-		tempBuffer := NewByteBuffer()
-		arg.Emit(tempBuffer)
-
-		v.stack.Push(tempBuffer.Bytes())
-
-		v.pp += arg.Size()
+		// Push immediate value
+		v.PushData(v.NextData())
 
 	case INS_DUP:
-		arg := &Data{}
+		// Decode the top of stack
+		arg := Data{}
 		arg.Decode(v.stack.Slice(0, -1))
 
-		// Use a bytebuffer to extract bytes
-		tempBuffer := NewByteBuffer()
-		arg.Emit(tempBuffer)
-		v.stack.Push(tempBuffer.Bytes())
+		// Push decoded data
+		v.PushData(arg)
 
 	case INS_POP:
-		arg := &Data{}
-		arg.Decode(v.stack)
-		v.stack = v.stack.Slice(arg.Size(), -1)
+		// Pop and discard
+		v.PopData()
 
 	case INS_EXIT:
 		v.Status = VM_DONE
 
 	case INS_RETURN:
+		// Set program pointer and pop callstack
 		v.pp = v.callStack[len(v.callStack)-1]
 		v.callStack = v.callStack[:len(v.callStack)-1]
 
 	case INS_CALL:
-		arg := &Data{}
-		arg.Decode(v.Prog.Slice(v.pp, -1))
-		v.pp += arg.Size()
-
-		// Use a bytebuffer to extract address
-		tempBuffer := NewByteBuffer()
-		tempBuffer.Push(arg.Value)
+		// Get address by converting
+		addr := int(binary.LittleEndian.Uint32(v.NextData().Value))
 
 		// Set return address
 		v.callStack = append(v.callStack, v.pp)
 
 		// Set new program pointer
-		v.pp = int(tempBuffer.Get32(0))
+		v.pp = addr
 
 	case INS_ADD:
-		arg0 := &Data{}
-		arg0.Decode(v.stack.Slice(0, -1))
-		v.stack = v.stack.Slice(arg0.Size(), -1)
+		// Pop arguments
+		arg0 := v.PopData()
+		arg1 := v.PopData()
 
-		arg1 := &Data{}
-		arg1.Decode(v.stack.Slice(0, -1))
-		v.stack = v.stack.Slice(arg1.Size(), -1)
-
+		// Decode values
 		v0 := int32(binary.LittleEndian.Uint32(arg0.Value))
 		v1 := int32(binary.LittleEndian.Uint32(arg1.Value))
 
-		res := NewInt32(v0 + v1)
-		tempBuffer := NewByteBuffer()
-		res.Emit(tempBuffer)
-		v.stack.Push(tempBuffer.Bytes())
+		// Generate and push new value
+		v.PushData(*NewInt32(v0 + v1))
 
 	case INS_JNE:
-		arg0 := &Data{}
-		arg0.Decode(v.stack)
-		v.stack = v.stack.Slice(arg0.Size(), -1)
-
-		arg1 := &Data{}
-		arg1.Decode(v.stack)
-		v.stack = v.stack.Slice(arg0.Size(), -1)
-
-		addr := &Data{}
-		addr.Decode(v.Prog.Slice(v.pp, -1))
-		v.pp += addr.Size()
+		// Pop arguments
+		arg0 := v.PopData()
+		arg1 := v.PopData()
+		addr := v.NextData()
 
 		// Jump if arguments aren't equal
-		if !arg0.Equals(arg1) {
-			// Use a bytebuffer to extract address
-			tempBuffer := NewByteBuffer()
-			tempBuffer.Push(addr.Value)
-
+		if !arg0.Equals(&arg1) {
 			// Set new program pointer
-			v.pp = int(tempBuffer.Get32(0))
+			v.pp = int(binary.LittleEndian.Uint32(addr.Value))
 		}
 
 	default:
